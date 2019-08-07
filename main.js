b0f23dc25e4aa950d6b1197e4013c56f9c60a9d2
SiaVash <picinfiniti@gmail.com>
2019-08-07 13:51:07 +0430

    frist commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res, next) {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(3000);
