1f8f2bf93e6da749ce086f859130a3623b9fe5d7
SiaVash <picinfiniti@gmail.com>
2019-08-10 10:12:09 +0430

    add template

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res, next) {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000);
